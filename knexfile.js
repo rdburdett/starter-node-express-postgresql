0b1db844b49e034324bab81ebeab9d7f96f4f8c5
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2022-04-19 22:04:26 -0400

    migrate knex development to elephantsql database

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,13 @@
+const path = require("path")
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
+
  module.exports = {
   development: {
     client: "postgresql",
     connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations")
+    },
   },
 };
